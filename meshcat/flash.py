ef6c20b4ba33b3bb70f807f9f8d180d561a3929d
Ben Meadors <benmmeadors@gmail.com>
2024-10-15 20:32:20 -0500

    Args fixes

--- meshcat/flash.py
+++ meshcat/flash.py
@@ -2,14 +2,14 @@ import esptool
 from nordicsemi.dfu.dfu import Dfu
 from nordicsemi.dfu.dfu_transport_serial import DfuTransportSerial
 
-def update_firmware_esp32(port, firmware_path):
-    command = ['--baud', '115200', 'write_flash', '0x10000', firmware_path]
+def update_firmware_esp32(port: str, firmware_path):
+    command = ['--port', port, '--baud', '115200', 'write_flash', '0x10000', firmware_path]
     print('Using esptool command %s' % ' '.join(command))
     esptool.main(command)
 
-def update_firmware_nrf52840(port, firmware_path):
+def update_firmware_nrf52840(port: str, firmware_path):
     """Program a device with bootloader that support serial DFU"""
-    serial_backend = DfuTransportSerial(port, 115200, flowcontrol=False, singlebank=False, touch=True)
+    serial_backend = DfuTransportSerial(port, 115200, touch=True)
     #serial_backend.register_events_callback(DfuEvent.PROGRESS_EVENT, update_progress)
     
     dfu = Dfu(firmware_path, dfu_transport=serial_backend)
